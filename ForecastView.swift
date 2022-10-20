e9523e9287fb885a66d798cc150260504b875460
Inheo <agch777505@gmail.com>
2022-10-20 17:50:42 +0300

    integrated to home view bottom sheet

---
+++ ForecastView.swift
@@ -0,0 +1,40 @@
+//
+//  ForecastView.swift
+//  Weather
+//
+//  Created by Даниял on 20.10.2022.
+//
+
+import SwiftUI
+
+struct ForecastView: View {
+    var body: some View {
+        ScrollView {
+            
+        }
+        .background(Color.bottomSheetBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 44))
+        .overlay {
+            Divider()
+                .blendMode(.overlay)
+                .background(Color.bottomSheetBorderTop)
+                .frame(maxHeight: .infinity, alignment: .top)
+                .clipShape(RoundedRectangle(cornerRadius: 44))
+        }
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.black.opacity(0.3))
+                .frame(width: 48, height: 5)
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding(.top, 10)
+        }
+    }
+}
+
+struct ForecastView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForecastView()
+            .background(Color.background)
+            .preferredColorScheme(.dark)
+    }
+}
