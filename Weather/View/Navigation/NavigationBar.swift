ccdad338bd1b1395cb338bdfcffac65f9f0deec1
Inheo <agch777505@gmail.com>
2022-10-29 11:00:32 +0300

    refactoring navigation bar

--- Weather/View/Navigation/NavigationBar.swift
+++ Weather/View/Navigation/NavigationBar.swift
@@ -10,62 +10,35 @@ import SwiftUI
 struct NavigationBar: View {
     @Environment(\.dismiss) var dismiss
     @Binding var searchText: String
-    @State var addNewCityAlertPresented = false
+    @State var addNewPlaceAlertPresented = false
     @State var newCity: String = ""
+    var height: CGFloat
     
     var addNewAddress: (String) -> Void
     
     var body: some View {
         ZStack {
-            VStack(spacing: 8) {
+            VStack(spacing: valueRelativeHeight(8)) {
                 HStack {
-                    Button {
-                        dismiss()
-                    } label: {
-                        HStack(spacing: 5) {
-                            Image(systemName: "chevron.left")
-                                .font(.system(size: 24).weight(.medium))
-                                .foregroundColor(.secondary)
-                            Text("Weather")
-                                .font(.system(size: 28).weight(.regular))
-                                .foregroundColor(.primary)
-                        }
-                        .frame(height: 44)
-                    }
-                    
+                    dissmisButton
                     Spacer()
-                    Button(action: { addNewCityAlertPresented = true }) {
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 28).weight(.regular))
-                    }
+                    addNewPlaceButton
                 }
-                
-                HStack(spacing: 2) {
-                    Image(systemName: "magnifyingglass")
-                    TextField("Search for a city or airport", text: $searchText)
-                }
-                .foregroundColor(.secondary)
-                .padding(.horizontal, 8)
-                .padding(.vertical, 10)
-                .frame(height: 36, alignment: .leading)
-                .background(Color.bottomSheetBackground, in: RoundedRectangle(cornerRadius: 10))
-                .innerShadow(shape: RoundedRectangle(cornerRadius: 10),
-                             color: .black.opacity(0.25),
-                             lineWidth: 2,
-                             offsetY: 2,
-                             blurRadius: 2)
+                searchPlace
             }
         }
-        .frame(height: 106, alignment: .top)
+        .frame(height: height, alignment: .top)
         .padding(.horizontal, 16)
-        .padding(.top, 49)
-        .backgroundBlur(radius: 20, opaque: true)
+        .padding(.top, topPadding)
+        .backgroundBlur(radius: blurRadius, opaque: true)
         .background(Color.navBarBackground)
         .frame(maxHeight: .infinity, alignment: .top)
         .ignoresSafeArea()
         .overlay {
-            if addNewCityAlertPresented {
-                AlertFindPlace(text: $newCity, isPresented: $addNewCityAlertPresented, title: "Write the name of the city") { placeName in
+            if addNewPlaceAlertPresented {
+                AlertFindPlace(text: $newCity,
+                               isPresented: $addNewPlaceAlertPresented,
+                               title: "Write the name of the city") { placeName in
                     withAnimation {
                         addNewAddress(newCity)
                     }
@@ -73,10 +46,59 @@ struct NavigationBar: View {
             }
         }
     }
+    
+    var dissmisButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            HStack(spacing: 5) {
+                Image(systemName: "chevron.left")
+                    .font(.system(size: chevronImageSize).weight(.medium))
+                    .foregroundColor(.secondary)
+                Text("Weather")
+                    .font(.system(size: baseSize).weight(.regular))
+                    .foregroundColor(.primary)
+            }
+            .frame(height: dismissButtonHeight)
+        }
+    }
+    
+    var addNewPlaceButton: some View {
+        Button(action: { addNewPlaceAlertPresented = true }) {
+            Image(systemName: "plus.circle")
+                .font(.system(size: baseSize).weight(.regular))
+        }
+    }
+    
+    var searchPlace: some View {
+        HStack(spacing: 2) {
+            Image(systemName: "magnifyingglass")
+            TextField("Search for a city", text: $searchText)
+        }
+        .foregroundColor(.secondary)
+        .padding(.horizontal, 8)
+        .padding(.vertical, valueRelativeHeight(10))
+        .frame(height: searchHeight, alignment: .leading)
+        .background(Color.bottomSheetBackground, in: baseShape)
+        .innerShadow(shape: baseShape,
+                     color: .black.opacity(0.25),
+                     lineWidth: 2,
+                     offsetY: 2,
+                     blurRadius: 2)
+    }
+    
+    // MARK: -
+    let blurRadius: CGFloat = 20
+    let topPadding = valueRelativeHeight(49)
+    let chevronImageSize = valueRelativeHeight(24)
+    let baseSize = valueRelativeHeight(28)
+    let searchHeight = valueRelativeHeight(36)
+    let dismissButtonHeight = valueRelativeHeight(44)
+    let baseShape = RoundedRectangle(cornerRadius: 10)
 }
 
 struct NavigationBar_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationBar(searchText: .constant(""), addNewAddress: {_ in })
+        NavigationBar(searchText: .constant(""), height: 110, addNewAddress: {_ in })
     }
 }
